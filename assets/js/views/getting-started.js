6af6689ccd237ba496ae27ee4ec95265e4c971fa
Sam Saccone <samccone@gmail.com>
2014-09-16 15:25:08 -0400

    Add getting-started toggle sections
    fixed #16

--- assets/js/views/getting-started.js
+++ assets/js/views/getting-started.js
@@ -2,8 +2,42 @@
   BK.Views = BK.Views || {};
 
   BK.Views.GettingStarted = Mn.LayoutView.extend({
+    events: {
+      "click .getting-started li": "switchOSTab"
+    },
+
+    ui: {
+      'platforms': '.block.platforms li',
+      'install': '.install-instructions'
+    },
+
     getTemplate: function() {
-      return templates['getting-started']
+      return templates['getting-started'];
+    },
+
+    getOS: function() {
+      if (~navigator.appVersion.indexOf("Win"))
+        return "windows";
+      if (~navigator.appVersion.indexOf("Mac"))
+        return "osx";
+      if (~navigator.appVersion.indexOf("X11") || ~navigator.appVersion.indexOf("Linux"))
+        return "linux";
+    },
+
+    switchOSTab: function(e) {
+      var $t       = $(e.currentTarget);
+      var template = templates[$t.data('platform')+"-install"];
+
+      this.ui.platforms.removeClass('active');
+      this.ui.install.html(template())
+
+      $t.addClass('active');
+    },
+
+    templateHelpers: function() {
+      return {
+        OS: this.getOS()
+      }
     }
   });
 })(window.BK = window.BK || {}, Backbone, Backbone.Marionette);
