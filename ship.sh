f062a24053f10797454725d5a25d08ce43098c07
Sam Saccone <samccone@gmail.com>
2014-09-18 13:07:08 -0400

    Switch to http deploy

--- ship.sh
+++ ship.sh
@@ -3,7 +3,7 @@ cd public
 git init
 git add .
 git commit -m "deploy"
-git remote add origin git@github.com:twosigma/beaker-notebook-www.git
+git remote add origin https://github.com/twosigma/beaker-notebook.git
 git push -f origin master:gh-pages
 rm -rf .git
 cd ..
