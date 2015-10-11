e1de387b813adfb09b81ef5234dedc7537586df8
Seth Krasnianski <seth@mojotech.com>
2015-10-10 23:08:21 -0400

    Remove templates directory when deploying

--- ship.sh
+++ ship.sh
@@ -4,6 +4,7 @@ git init
 rm -f ship.conf
 find . -type f -name '#*#' -delete
 find . -type f -name '*~' -delete
+rm -rf templates
 git add .
 git commit -m "deploy"
 git remote add origin https://github.com/twosigma/beaker-notebook-www.git
