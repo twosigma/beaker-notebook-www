1b4ca936c49ad78eacaed25e81eea40ba11f3389
Seth Krasnianski <seth@mojotech.com>
2015-10-10 20:29:25 -0400

    Close email modal when clicking outside

--- assets/js/views/modal.js
+++ assets/js/views/modal.js
@@ -2,6 +2,17 @@
   BK.Views = BK.Views || {};
 
   BK.Views.Modal = Mn.ItemView.extend({
+    initialize: function() {
+      $(document).on('click.modal', this.clickOutside.bind(this));
+      this.clickedOutside = 0;
+    },
+    clickOutside: function(e) {
+      if (!$(e.target).hasClass('mc-field-group') && this.clickedOutside > 0) {
+        this.destroy();
+      }
+
+      this.clickedOutside++;
+    },
     events: {
       'submit #mc-embedded-subscribe-form': "destroy",
       'click .close': "destroy"
@@ -13,6 +24,7 @@
       $('#modal').show();
     },
     onDestroy: function() {
+      $(document).off('click.modal');
       $('#modal').hide();
     }
   });
