43b23a4310fa29b23fc9d9c48883fb34431b2b26
Scott Draves <spot@draves.org>
2016-03-07 19:32:57 -0500

    add v1.5.2 release

--- assets/js/util.js
+++ assets/js/util.js
@@ -2,11 +2,11 @@
   BK.getDownloadLink = function(platform) {
     switch (platform || BK.getPlatform()) {
       case 'windows':
-        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.1-0-g6e6bdd3-windows.zip"; 
+        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.2-0-g34d512f-windows.zip"; 
       case 'osx':
-        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.1-0-g6e6bdd3-mac.dmg";
+        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.2-0-g34d512f-mac.dmg";
       default:
-        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.1-0-g6e6bdd3-ubuntu.zip";
+        return "http://d299yghl10frh5.cloudfront.net/beaker-notebook-1.5.2-0-g34d512f-ubuntu.zip";
     }
   }
 
